5c338042288245a41609f15c68298ce0a9d62b41
levchenko-ivan <ivan97111@yandex.ru>
2025-03-10 14:40:53 +0300

    fix: welcome page

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use MoonShine\Laravel\Models\MoonshineUser;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        MoonShineUser::query()->create([
+            'login' => 'admin@mail.ru',
+            'email' => 'admin@mail.ru',
+            'password' => Hash::make(12345)
         ]);
     }
 }
