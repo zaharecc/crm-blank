5cd6412c9b397638892e72e3942b2c2331bb33ed
zaharecc <85930442+zaharecc@users.noreply.github.com>
2025-09-16 09:20:25 +0300

    Add files via upload

---
+++ lang.php
@@ -0,0 +1,9 @@
+<?php
+if(!is_dir('lang/vendor/moonshine/ru')) {mkdir('lang/vendor/moonshine/ru',0755,true); 
+copy('vendor/zaharecc/moonshine/src/Laravel/lang/vendor/moonshine/ru/auth.php', 'lang/vendor/moonshine/ru/auth.php'); 
+copy('vendor/zaharecc/moonshine/src/Laravel/lang/vendor/moonshine/ru/pagination.php', 'lang/vendor/moonshine/ru/pagination.php'); 
+copy('vendor/zaharecc/moonshine/src/Laravel/lang/vendor/moonshine/ru/ui.php', 'lang/vendor/moonshine/ru/ui.php'); 
+copy('vendor/zaharecc/moonshine/src/Laravel/lang/vendor/moonshine/ru/validation.php', 'lang/vendor/moonshine/ru/validation.php');
+}
+
+?>
